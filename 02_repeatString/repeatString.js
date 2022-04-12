12dab93756fd6280714766c7607be8c07d1894e8
minidiscs <shanedbutler@gmail.com>
2022-04-12 18:38:26 -0400

    completed exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
+    let shout = '';
+    for (let index = 0; index < num; index++) {
+        shout += string;
+     }
+     return shout;
 
 };
 
