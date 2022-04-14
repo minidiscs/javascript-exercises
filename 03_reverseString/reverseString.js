128ff15cac4a514b2515e49dcb9638a46bb6c7f0
minidiscs <shanedbutler@gmail.com>
2022-04-14 07:29:55 -0400

    exercise 3 solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(input) {
 
+    const splitString = input.split("");
+    const reverseArray = splitString.reverse();
+    const joinArray = reverseArray.join("");
+
+    return joinArray;
 };
 
 // Do not edit below this line
