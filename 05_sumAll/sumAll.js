39778f921be0194241ff1f087d28bc0c5eebc0ad
minidiscs <shanedbutler@gmail.com>
2022-07-07 16:44:05 -0400

    solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let finalSum = 0
+    for (let index = a; index <= b; index++) {
+        finalSum += a
+        a++
+    }
+    return finalSum
+}
 
-};
+console.log(sumAll(1, 4))
 
 // Do not edit below this line
 module.exports = sumAll;
