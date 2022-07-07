87ee668c077614beeaaf47521643eab55798aaab
minidiscs <shanedbutler@gmail.com>
2022-07-07 17:14:31 -0400

    solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  const cTemp = (temp - 32) * (5/9)
+  const roundedTemp = Math.round(cTemp * 10) / 10
+  return roundedTemp
+}
 
-};
-
-const ctof = function() {
-
-};
+const ctof = function(temp) {
+  const fTemp = temp * (9/5) + 32
+  const roundedTemp = Math.round(fTemp * 10) / 10
+  return roundedTemp
+}
 
 // Do not edit below this line
 module.exports = {
